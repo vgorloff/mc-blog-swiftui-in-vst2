d8ab08837afcc8032ee47a156cb18bf2577990c6
Vlad Gorlov <volodymyr.gorlov@gmail.com>
2020-03-30 12:16:59 +0200

    Created Xcode project.

---
+++ AttenuatorVST2/AttenuatorVST2.h
@@ -0,0 +1,19 @@
+//
+//  AttenuatorVST2.h
+//  AttenuatorVST2
+//
+//  Created by Vlad Gorlov on 30.03.20.
+//  Copyright © 2020 Vlad Gorlov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AttenuatorVST2.
+FOUNDATION_EXPORT double AttenuatorVST2VersionNumber;
+
+//! Project version string for AttenuatorVST2.
+FOUNDATION_EXPORT const unsigned char AttenuatorVST2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AttenuatorVST2/PublicHeader.h>
+
+
---
+++ AttenuatorVST2UI/AttenuatorVST2UI.h
@@ -0,0 +1,19 @@
+//
+//  AttenuatorVST2UI.h
+//  AttenuatorVST2UI
+//
+//  Created by Vlad Gorlov on 30.03.20.
+//  Copyright © 2020 Vlad Gorlov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AttenuatorVST2UI.
+FOUNDATION_EXPORT double AttenuatorVST2UIVersionNumber;
+
+//! Project version string for AttenuatorVST2UI.
+FOUNDATION_EXPORT const unsigned char AttenuatorVST2UIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AttenuatorVST2UI/PublicHeader.h>
+
+
